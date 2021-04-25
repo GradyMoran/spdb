562901f9efeea0a7cb5f8df151f31ee816f3ad3b
GradyMoran <gradymoran95@gmail.com>
2021-04-25 19:09:46 -0400

    Partially fixed errors regarding adding signal handler to asyncio loop

--- spdb.py
+++ spdb.py
@@ -1,6 +1,7 @@
 import asyncio
 import discord
 from dotenv import load_dotenv
+import functools
 import nest_asyncio
 import os
 import signal
@@ -25,7 +26,7 @@ async def on_message(message):
     if message.content in ["+portfolio", "+p"]:
         await message.channel.send(prices_printout())
 
-def sigusr1_handler(signum, frame):
+def sigusr1_handler(loop):
     asyncio.get_event_loop().run_until_complete(client.get_channel(SIGNAL_PRINT_CHANNEL).send(prices_printout()))
 
 def prices_printout():
@@ -52,7 +53,9 @@ def prices_printout():
                             (' '*(11-len('%.2f' % cp))) + "$" + '%.2f' % op +
                             (' '*(11-len('%.2f' % op))) + (" " if cp > op else "") + '%.2f' % change + "%")
     return "```\n" + '\n'.join(return_lines) + "```"
-
-nest_asyncio.apply()
-signal.signal(signal.SIGUSR1, sigusr1_handler)
-client.run(DISCORD_TOKEN)
+    
+#Adding nest_asyncio fixes the "This event loop is already running" error, but breaks on_message.
+#nest_asyncio.apply()
+loop = asyncio.get_event_loop()
+loop.add_signal_handler(signal.SIGUSR1, functools.partial(sigusr1_handler, loop))
+loop.run_until_complete(client.start(DISCORD_TOKEN))
